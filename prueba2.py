23799f5ccce40b8cffa37765d479e159a9409af5
Marina Ramajo Lázaro <mramajol06@gmail.com>
2022-12-04 22:57:50 +0100

    Array bien creado
    Imagenes del array bien creado, con posiciones bien establecidas.

--- prueba2.py
+++ prueba2.py
@@ -1,9 +1,8 @@
 #Importacion de librerias necesarias
 import tkinter as tk
 from tkinter import PhotoImage, Label, Toplevel, ttk
-import random
 
-def crea_casillas(event, fondo_casilla, pastel):
+def crea_platos(event, fondo_casilla, pastel1,pastel2, pastel3, pastel4, pastel5):
     # print(str(event.x) +","+ str(event.y))
     if ((422 <= event.x <= 522) and (212 <= event.y <= 312)) or \
             ((422 <= event.x <= 522) and (586 <= event.y <= 686)) or \
@@ -14,38 +13,49 @@ def crea_casillas(event, fondo_casilla, pastel):
             ((774 <= event.x <= 874) and (586 <= event.y <= 686)) or \
             ((774 <= event.x <= 874) and (212 <= event.y <= 312)):
         #Si el usuario hace clic sobre alguna zona activa, creamos una ventana secundaria que abre esa casilla
-        casilla(fondo_casilla, pastel)
+        casilla(fondo_casilla, pastel1, pastel2, pastel3, pastel4, pastel5)
 
-def casilla(fondo_casilla, pastel):
+def casilla(fondo_casilla, foto_pastel1,foto_pastel2 ,foto_pastel3 ,foto_pastel3_correcto,foto_pastel_cereza):
     plato = Toplevel(ventana)
     plato.resizable(width=False, height=False)
     plato.title("Casilla Verde")
-    plato.geometry("700x700+350+0")
+    plato.geometry("500x500+430+22")
     fondo = Label(plato, image=fondo_casilla)
     fondo.place(relwidth=1, relheight=1)
     #Registramos los clics que haga el usuario en la casilla para poder hacer el recuento correctamente
-    plato.bind("<Button-1>", lambda event, pastel=pastel: cuenta_puntos(event, pastel, plato))
+    plato.bind("<Button-1>", lambda event,foto_pastel1=foto_pastel1, foto_pastel2=foto_pastel2,
+    foto_pastel3=foto_pastel3, foto_pastel3_correcto=foto_pastel3_correcto,
+    foto_pastel_cereza=foto_pastel_cereza:
+    inserta_pasteles(event, foto_pastel1,foto_pastel2 ,foto_pastel3 ,foto_pastel3_correcto,foto_pastel_cereza, plato))
 
-
-def cuenta_puntos(event, pastel, plato):
+def inserta_pasteles(event, foto_pastel1,foto_pastel2 ,foto_pastel3 ,foto_pastel3_correcto,foto_pastel_cereza, plato):
     #print(str(event.x) + "," + str(event.y))
-    array_pasteles = ['Pasteles/1.png', 'Pasteles/2.png', 'Pasteles/3.png', 'Pasteles/4.png', 'Pasteles/5.png', 'Pasteles/6.png', 'Pasteles/7.png', 'Pasteles/8.png']
-    if ((0 <= event.x <= 350) and (0 <= event.y <= 233)):
-        n=random.randint(0,2)
-        pastel.config(file=array_pasteles[n])
-        p1 = Label(plato, image=pastel)
-        p1.place(x=event.x, y=event.y)
-    elif ((351 <= event.x <= 700) and (0 <= event.y <= 233)):
-        n = random.randint(3, 5)
-        pastel.config(file=array_pasteles[n])
-        p2 = Label(plato, image=pastel)
-        p2.place(x=event.x, y=event.y)
-    elif ((0 <= event.x <= 350) and (234<= event.y <= 466)):
-        print("Pasteles de 3 capas")
-    elif ((351 <= event.x <= 700) and (234 <= event.y <= 466)):
-        print("Pastel de 3 capas correcto")
-    elif ((0 <= event.x <= 700) and (466 <= event.y <= 700)):
-        print("Pastel completo")
+    array_pasteles = ['Pasteles/1.png', 'Pasteles/5.png', 'Pasteles/7.png', 'Pasteles/8.png', 'Pasteles/9.png']
+    if ((0 <= event.x <= 250) and (0 <= event.y <= 166)):
+        foto_pastel1.config(file=array_pasteles[0])
+        pastel1 = Label(plato, image=foto_pastel1)
+        pastel1.place(x=event.x, y=event.y)
+
+    elif ((251 <= event.x <= 500) and (0 <= event.y <= 166)):
+        foto_pastel2.config(file=array_pasteles[1])
+        pastel2 = Label(plato, image=foto_pastel2)
+        pastel2.place(x=event.x, y=event.y)
+    elif ((0 <= event.x <= 250) and (167<= event.y <= 332)):
+        foto_pastel3.config(file=array_pasteles[2])
+        pastel3 = Label(plato, image=foto_pastel3)
+        pastel3.place(x=event.x, y=event.y)
+    elif ((251 <= event.x <= 500) and (167 <= event.y <= 332)):
+        foto_pastel3_correcto.config(file=array_pasteles[3])
+        pastel3_correcto = Label(plato, image=foto_pastel3_correcto)
+        pastel3_correcto.place(x=event.x, y=event.y)
+    elif ((0 <= event.x <= 500) and (332 <= event.y <= 500)):
+        foto_pastel_cereza.config(file=array_pasteles[4])
+        pastel_completo = Label(plato, image=foto_pastel_cereza)
+        pastel_completo.place(x=event.x, y=event.y)
+
+def cuenta_puntos_plato(contador):
+    pass
+
 
 # Creamos la ventana principal
 ventana = tk.Tk()
@@ -57,11 +67,17 @@ ventana.geometry("1300x700+30+0")  # dimensiones de la ventana
 imgfondo = PhotoImage(file="tablero.png")
 fondo = Label(ventana, image=imgfondo)
 fondo.place(relwidth=1, relheight=1)
-#fondo.place(x = 422, y = 0)
 
-#Establecemos un array con imagenes de los diferentes pasteles
-pastel_casilla = PhotoImage()
+#Creamos photo image de los diferentes tipos de pasteles que contabilizan puntos
+pastel_capa1 = PhotoImage()
+pastel_capa2 = PhotoImage()
+pastel_capa3 = PhotoImage()
+pastel_capa_ordenada = PhotoImage()
+pastel_cereza= PhotoImage()
 
 # capturamos el clic izquierdo del raton y se lo pasamos a las casillas
-ventana.bind("<Button-1>", lambda event, imgfondo2 = PhotoImage(file="fondo3.png"), pastel=pastel_casilla: crea_casillas(event, imgfondo2, pastel))
+ventana.bind("<Button-1>", lambda event, imgfondo2 = PhotoImage(file="fondo3.png"),
+pastel1=pastel_capa1, pastel2=pastel_capa2, pastel3=pastel_capa3, pastel4=pastel_capa_ordenada,
+pastel5=pastel_cereza: crea_platos(event, imgfondo2, pastel1, pastel2, pastel3, pastel4, pastel5))
+
 ventana.mainloop()
