d9da6ff8ebd62f52d27660c9534a38afee64eeeb
marinarl06 <mramajol06@gmail.com>
2023-01-11 23:00:37 +0100

    Funcionalidad leyendo de xml
    Ya se leen los datos del xml y se trabaja con ellos

--- leer_xml.py
+++ leer_xml.py
@@ -1,19 +1,70 @@
+"""import xmltodict
+
+# Abre el archivo XML
+with open("xmlprueba.xml", "r") as archivo:
+  # Convierte el contenido del archivo a un diccionario
+  datos = xmltodict.parse(archivo.read())
+
+# Accede a los datos del diccionario como lo harías con cualquier otro diccionario de Python
+print(datos)
+
+acciones=datos["EurobotSpain"]["acciones"]
+
+print(acciones)"""
+
 import xml.etree.ElementTree as ET
+tree = ET.parse('xmlprueba.xml')
+root = tree.getroot()
+listaAcciones=[]
 
-class Archivo:
+#print(root.tag,root.attrib)"""
+for child in root:
+    print(child.tag, child.attrib)
 
-    def leer_xml(self):
-        # Parse the XML file
-        tree = ET.parse('temp_2023.xml')
-        # Get the root element of the XML document
-        root = tree.getroot()
-        # Iterate over the child elements of the root element
-        for child in root:
-            # Print the tag and text for each child element
-            print(child.tag, child.text)
-            if child.tag == 'element1':
-            # Assign the text of the child element to a variable
-                variable1 = child.text
-            elif child.tag == 'element2':
-                # Assign the attribute of the child element to a variable
-                variable2 = child.attrib['attribute']
+    for childs in child:
+        if child.tag =='acciones':
+            listaAcciones.append(childs)
+            print("el tag es " + childs.tag, childs.attrib)
+            print(childs.attrib['nombre'])
+        elif child.tag =='resultados':
+            print("el tag es " + childs.tag, childs.attrib)
+            print(childs.attrib['nombre'])
+
+for child in listaAcciones:
+    print(child.attrib)
+'''
+from lxml import etree
+
+xml_file = 'archivo.xml'
+
+# parsear el archivo XML
+xml_tree = etree.parse('xmlprueba.xml')
+
+def print_attribs(element, depth=0):
+    # imprimir el nombre del elemento y sus atributos
+    print(element.tag, element.attrib)
+    # iterar sobre los hijos del elemento
+    for child in element.getchildren():
+        # llamar a la función recursivamente para cada hijo
+        print_attribs(child, depth+1)
+
+# llamar la función con el elemento raíz
+print_attribs(xml_tree.getroot())
+
+def create_dict(element):
+    # crear un diccionario para el elemento actual
+    element_dict = {'tag': element.tag, 'attrib': element.attrib}
+    element_dict["text"] = element.text
+    # crear una lista para almacenar los diccionarios de los hijos
+    children_list = []
+    # iterar sobre los hijos del elemento
+    for child in element.getchildren():
+        # llamar a la función recursivamente para cada hijo
+        children_list.append(create_dict(child))
+    if children_list:
+        element_dict['children'] = children_list
+    return element_dict
+
+root_dict = create_dict(xml_tree.getroot())
+print(root_dict)
+'''
--- principal.py
+++ principal.py
@@ -1,10 +1,8 @@
 #Importacion de librerias necesarias
 import tkinter as tk
-from tkinter import PhotoImage, Canvas
-import os
+from tkinter import PhotoImage, Canvas, Toplevel
+import xml.etree.ElementTree as ET
 
-plato1_azul = {'x1': [422, 472, 422, 472, 422], 'x2': [472, 522, 472, 522, 522], 'y1': [0, 0, 32, 32],
-                   'y2': [32, 32, 66, 66]}
 contador_clics=0
 contador_clics_c1=0
 contador_clics_c2=0
@@ -12,78 +10,61 @@ contador_clics_c3=0
 contador_clics_ord=0
 contador_clics_cereza=0
 
+listaAcciones=[]
+listaResultados=[]
+
+def leer_xml():
+    global listaAcciones
+    tree = ET.parse('xmlprueba.xml')
+    root = tree.getroot()
+    for child in root:
+        #print(child.tag, child.attrib)
+
+        for childs in child:
+            if child.tag =='acciones':
+                #print(childs.attrib)
+                listaAcciones.append(childs.attrib)
+            elif child.tag =='resultados':
+                listaResultados.append(childs.attrib)
+
 def instrucciones():
     '''
     Funcion para explicar donde debe hacer clic el usuario en función de los puntos
     NOTA: No sé si mostrarlas en la parte izquierda de la ventana o crear una ventana principal que muestre las
     instrucciones y al pulsar un boton OK nos muestre ya el tablero
     '''
-    canvas.create_text(200, 90, text="Contamos con la siguiente figura en cada plato del tablero:")
-    canvas.create_line(100, 110, 200, 110, width=5)  #
-    canvas.create_line(100, 130, 200, 130, width=5)  #
-    canvas.create_line(100, 150, 200, 150, width=5)  #
-    canvas.create_line(100, 170, 200, 170, width=5)
-    canvas.create_line(100, 110, 100, 170, width=5)  # VERTICAL
-    canvas.create_line(200, 110, 200, 170, width=5)  # VERTICAL
-    canvas.create_line(150, 110, 150, 150, width=5)  # VERTICAL
+    newWindow=Toplevel(ventana)
+    newWindow.title("Instrucciones")
+    newWindow.geometry("700x500")
 
+    canvas2 = Canvas(newWindow, width=700, height=500)
+    canvas2.pack()
 
+    canvas2.create_text(350, 90, text="Contamos con la siguiente figura en cada plato del tablero:")
+    canvas2.create_line(300, 110, 400, 110, width=5)  #
+    canvas2.create_line(300, 130, 400, 130, width=5)  #
+    canvas2.create_line(300, 150, 400, 150, width=5)  #
+    canvas2.create_line(300, 170, 400, 170, width=5)
+    canvas2.create_line(300, 110, 300, 170, width=5)  # VERTICAL
+    canvas2.create_line(400, 110, 400, 170, width=5)  # VERTICAL
+    canvas2.create_line(350, 110, 350, 150, width=5)  # VERTICAL
+    canvas2.create_text(330, 250, text=" Clica en el primer cuadrado para puntuar pasteles de una capa\n"
+                                      "En el segundo para puntuar pasteles de dos capas\n"
+                                      "En el tercer cuadrado para puntuar pasteles de 3 capas que no siguen un orden\n"
+                                      "En el cuarto para puntuar los de tres capas que siguen un orden\n"
+                                      "Y en el utlimos rectangulo para los pasteles que tienen una cereza.\n")
 
-def inserta_lineas():
+def inserta_lineas(acciones):
     '''
-    Función que genera las zonas activas
+    Función que pinta las zonas activas en el lienzo
     '''
-    #PL1A
-    canvas.create_line(472,0,472,66, width=5) #VERTICAL
-    canvas.create_line(422, 66, 522, 66, width=5)
-    canvas.create_line(422, 32, 522, 32, width=5)
 
-    #PL1V
-    canvas.create_line(472, 202, 472, 268, width=5) #VERTICAL
-    canvas.create_line(422, 268, 522, 268, width=5)
-    canvas.create_line(422, 235, 522, 235, width=5)
+    for i in range(len(acciones)):
+        canvas.create_rectangle(acciones[i]["x1"], acciones[i]["y1"], acciones[i]["x2"], acciones[i]["y2"], width=3,
+                                outline='black')
 
-    # PL2A
-    canvas.create_line(472, 373, 472, 439, width=5)  # VERTICAL
-    canvas.create_line(422, 406, 522, 406, width=5)
-    canvas.create_line(422, 439, 522, 439, width=5)
 
-    # PL2V
-    canvas.create_line(472, 576, 472, 642, width=5)  # VERTICAL
-    canvas.create_line(422, 609, 522, 609, width=5)
-    canvas.create_line(422, 642, 522, 642, width=5)
-
-    #PL3A
-    canvas.create_line(585, 576, 585, 642, width=5)  # VERTICAL
-    canvas.create_line(535, 609, 635, 609, width=5)
-    canvas.create_line(535, 642, 635, 642, width=5)
-
-    # PL3V
-    canvas.create_line(710, 576, 710, 642, width=5)  # VERTICAL
-    canvas.create_line(660, 609, 760, 609, width=5)
-    canvas.create_line(660, 642, 760, 642, width=5)
-
-    # PL4A
-    canvas.create_line(824, 576, 824, 642, width=5)  # VERTICAL
-    canvas.create_line(774, 609, 874, 609, width=5)
-    canvas.create_line(774, 642, 874, 642, width=5)
-
-    # PL4V
-    canvas.create_line(824, 372, 824, 438, width=5)  # VERTICAL
-    canvas.create_line(774, 405, 874, 405, width=5)
-    canvas.create_line(774, 438, 874, 438, width=5)
-
-    # PL5A
-    canvas.create_line(824, 202, 824, 268, width=5)  # VERTICAL
-    canvas.create_line(774, 232, 874, 232, width=5)
-    canvas.create_line(774, 268, 874, 268, width=5)
-
-    # PL5V
-    canvas.create_line(824, 0, 824, 66, width=5)  # VERTICAL
-    canvas.create_line(774, 33, 874, 33, width=5)
-    canvas.create_line(774, 66, 874, 66, width=5)
-
-def calcula_puntos(event):
+def calcula_puntos(event, acciones):
     '''
     Funcion que cuenta los puntos parciales realizados
     '''
@@ -94,38 +75,49 @@ def calcula_puntos(event):
     global contador_clics_ord
     global contador_clics_cereza
 
-    if(422 <= event.x < 472 and 0 <= event.y <33):
-        contador_clics_c1+=1
-        if (contador_clics_c1 >= 3):
-            contador_clics_c1=3
-    elif (472 <= event.x < 522 and 0 <= event.y <33):
-        contador_clics_c2+=2
-        if (contador_clics_c2 >= 6):
-            contador_clics_c2=6
-    elif (422 <= event.x < 472 and 33 <= event.y <66):
-        contador_clics_c3+=3
-        if (contador_clics_c3 >= 9):
-            contador_clics_c3=9
-    elif (472 <= event.x < 522 and 33 <= event.y <66):
-        contador_clics_ord+=7
-        if (contador_clics_ord >= 21):
-            contador_clics_ord=21
-    elif (422 <= event.x < 522 and 66 <= event.y < 100):
-        contador_clics_cereza += 3
-        if (contador_clics_cereza >= 9):
-            contador_clics_cereza=9
+    for i in range(len(acciones)):
+        if (int(acciones[i]['x1']) <= event.x < int(acciones[i]['x2']) and
+                int(acciones[i]['y1']) <= event.y < int(acciones[i]['y2'])):
+            if str(acciones[i]['nombre']).__contains__("1_capa"):
+                contador_clics_c1 += 1
+                if (contador_clics_c1 >= (int(acciones[i]['max']))):
+                    contador_clics_c1 = 3
+            elif str(acciones[i]['nombre']).__contains__("2_capas"):
+                contador_clics_c2 += 2
+                if (contador_clics_c2 >= (int(acciones[i]['max']))*2):
+                    contador_clics_c2 = 6
+            elif str(acciones[i]['nombre']).__contains__("3_capas"):
+                contador_clics_c3 += 3
+                if (contador_clics_c3 >= (int(acciones[i]['max']))*3):
+                    contador_clics_c3 = 9
+            elif str(acciones[i]['nombre']).__contains__("orden"):
+                contador_clics_ord += 7
+                if contador_clics_ord >= (int(acciones[i]['max']))*7:
+                    contador_clics_ord = 21
+            elif str(acciones[i]['nombre']).__contains__("cereza"):
+                contador_clics_cereza += 3
+                if (contador_clics_cereza >= (int(acciones[i]['max']))*3):
+                    contador_clics_cereza = 9
 
     contador_clics = (contador_clics_c1 + contador_clics_c2 + contador_clics_c3 + contador_clics_ord
                       + contador_clics_cereza)
-    muestra_puntos(contador_clics)
+    muestra_puntos(contador_clics, listaResultados)
     datos_a_txt(contador_clics_c1, contador_clics_c2, contador_clics_c3, contador_clics_ord, contador_clics_cereza, contador_clics)
 
-def muestra_puntos(puntos):
-    canvas.itemconfig(texto, text=str(puntos))
+def muestra_puntos(puntos, resultado):
+    for i in range(len(resultado)):
+        texto= canvas.create_text((int(resultado[i]["x2"])), (int(resultado[i]["y2"])), text=puntos)
+        canvas.itemconfig(texto, text=puntos)
+
+
+def modifica_etiqueta_puntos(resultado):
+    for i in range(len(resultado)):
+        canvas.create_text((int(resultado[i]["x1"])),(int(resultado[i]["y1"])), text=str(resultado[i]["nombre"]))
 
 def datos_a_txt(contador_clics_c1, contador_clics_c2, contador_clics_c3, contador_clics_ord, contador_clics_cereza, contador_clics):
     '''
     Funcion que exporta los datos del partido a un txt
+    NOTA: Poner como nombre del  txt los datos del partido, como nombre equipos y fecha de disputa
     '''
 
     archivo=open("partido.txt","w")
@@ -138,9 +130,6 @@ def datos_a_txt(contador_clics_c1, contador_clics_c2, contador_clics_c3, contado
     archivo.write("Pasteles con cereza: "+ str(contador_clics_cereza/3)+"\n")
     archivo.write("Puntos parciales en el plato 1: "+ str(contador_clics)+ "\n")
 
-
-
-
 # Creamos la ventana principal
 ventana = tk.Tk()
 ventana.resizable(width=False, height=False)  # para que la ventana no pueda redimensionarse
@@ -151,16 +140,31 @@ canvas=Canvas(ventana,width=1300,height=700)
 canvas.pack()
 
 imgfondo = PhotoImage(file="tablero.png") #Cargamos la imagen del tablero en la ventana
-canvas.create_image(422,0, anchor=tk.NW, image=imgfondo)
+canvas.create_image(422,0, anchor=tk.NW, image=imgfondo) #ubicamos el lienzo en la posicion deseada
 
-ventana.geometry("+20+0")
+ventana.geometry("+20+0") #ubicacion de la ventana
 
-#ETIQUETAS DE TEXTO
-canvas.create_text(350,33, text="Puntuacion parcial")
-texto=canvas.create_text(350,50, text=0)
+#Boton para ver las instrucciones de la aplicación (?)
+boton_intrucciones = tk.Button(ventana, text="Instrucciones", command=instrucciones) #creamos el boton
+boton_intrucciones.pack()
 
-instrucciones()
-inserta_lineas()
-canvas.bind('<Button-1>', calcula_puntos)
+leer_xml()
+inserta_lineas(listaAcciones)
+
+canvas.bind("<Button-1>",  lambda event, acciones=listaAcciones: calcula_puntos(event, listaAcciones))
+
+
+e1=canvas.create_text((int(listaResultados[0]["x2"])),(int(listaResultados[0]["y2"])),text=0)
+e2=canvas.create_text((int(listaResultados[1]["x2"])),(int(listaResultados[1]["y2"])),text=0)
+e3=canvas.create_text((int(listaResultados[2]["x2"])),(int(listaResultados[2]["y2"])),text=0)
+e4=canvas.create_text((int(listaResultados[3]["x2"])),(int(listaResultados[3]["y2"])),text=0)
+e5=canvas.create_text((int(listaResultados[4]["x2"])),(int(listaResultados[4]["y2"])),text=0)
+e6=canvas.create_text((int(listaResultados[5]["x2"])),(int(listaResultados[5]["y2"])),text=0)
+e7=canvas.create_text((int(listaResultados[6]["x2"])),(int(listaResultados[6]["y2"])),text=0)
+e8=canvas.create_text((int(listaResultados[7]["x2"])),(int(listaResultados[7]["y2"])),text=0)
+e9=canvas.create_text((int(listaResultados[8]["x2"])),(int(listaResultados[8]["y2"])),text=0)
+e10=canvas.create_text((int(listaResultados[9]["x2"])),(int(listaResultados[9]["y2"])),text=0)
+
+#print(listaAcciones[1])
 
 ventana.mainloop()
